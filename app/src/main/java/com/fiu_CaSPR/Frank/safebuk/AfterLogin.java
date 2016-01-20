debda9f5157e0cc3f8fd308e618f935e4f0cb9c7
sajedul karim talukder <sajib.cse.buet@gmail.com>
2016-01-19 20:52:47 -0500

    Temboo Information Fixed

--- app/src/main/java/com/fiu_CaSPR/Frank/safebuk/AfterLogin.java
+++ app/src/main/java/com/fiu_CaSPR/Frank/safebuk/AfterLogin.java
@@ -87,7 +87,7 @@ public class AfterLogin extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Toast.makeText(AfterLogin.this, "After Login Page Loaded", Toast.LENGTH_SHORT).show();
+        Toast.makeText(AfterLogin.this, "After Login Page Loaded", Toast.LENGTH_SHORT).show();
 
         if (isNetworkAvailable()) {
             Toast.makeText(this, "Loading Please Wait", Toast.LENGTH_LONG);
@@ -99,11 +99,13 @@ public class AfterLogin extends ActionBarActivity {
 
 
         // Initialize the WebView
+        //FacebookRegexPatternPool.id = "10200577169468421";
         webView = (WebView) findViewById(R.id.webView);
         webSettings =  webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.getSettings().setUseWideViewPort(false);
-        //Toast.makeText(AfterLogin.this, "Access Token: " + AccessToken.getCurrentAccessToken().getToken(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(AfterLogin.this, "Access Token: " + AccessToken.getCurrentAccessToken().getToken(), Toast.LENGTH_SHORT).show();
+        //AfterLogin.webView.loadUrl("https://www.facebook.com/10200577169468421");
         new FacebookGetUserInfoTask().execute(null, null, null);
         /*// Initialize OAuth helper
         oAuthHelper = new FacebookOAuthHelper(webView, FORWARDING_URL, this);
@@ -131,6 +133,8 @@ public class AfterLogin extends ActionBarActivity {
             // on the website m.facebook.com yet, and if the user has logged in yet. We know if the user has logged in or not,
             // by if we have their id yet. If we have their id, that means we can get the user's username from the url
             public void onPageFinished(WebView view, String url) {
+                Toast.makeText(AfterLogin.this, "OnPageFinished Called", Toast.LENGTH_SHORT).show();
+
                 if (webView != null) {
                     String s = webView.getUrl();
                     //Toast.makeText(AfterLogin.this, "URL LOADED: " + s, Toast.LENGTH_SHORT).show();
@@ -158,6 +162,7 @@ public class AfterLogin extends ActionBarActivity {
     public void start()
     {
         Intent intent = new Intent(getApplicationContext(),FetchData.class);
+        Toast.makeText(AfterLogin.this, "Now calling FetchData to save files", Toast.LENGTH_SHORT).show();
         startActivity(intent);
     }
 
@@ -186,94 +191,3 @@ public class AfterLogin extends ActionBarActivity {
 }
 
 
-class FacebookGetUserInfoTask extends AsyncTask<Void, Void, String> {
-    private AccessToken accesstoken;
-    // Replace with your Temboo credentials.
-    private static final String TEMBOO_ACCOUNT_NAME = "fiuseclab";
-    private static final String TEMBOO_APP_KEY_NAME = "safebuk";
-    private static final String TEMBOO_APP_KEY_VALUE = "Xa31WNulJ4EzrlkT08rkyNkiiUvzWNvT";
-    String AccessTokenString=AccessToken.getCurrentAccessToken().getToken();
-    private TembooSession session;
-    @Override
-    protected String doInBackground(Void... params) {
-        try {
-            try {
-
-                session = new TembooSession(TEMBOO_ACCOUNT_NAME, TEMBOO_APP_KEY_NAME, TEMBOO_APP_KEY_VALUE);
-            } catch (TembooException te) {
-                Log.d("", "Error1");
-                Log.e("FacebookOAuthHelpder", te.getMessage());
-            }
-            FacebookRegexPatternPool.accessToken = AccessTokenString;
-            // Get user info. Instantiate the choreo, using a session object.
-            User userChoreo = new User(session);
-
-            // Get an InputSet object for the Facebook.Reading.User choreo.
-            User.UserInputSet userInputs = userChoreo.newInputSet();
-
-            // Pass access token to the Facebook.Reading.User choreo.
-            Log.d("", "Access Token: " + AccessTokenString);
-            AfterLogin.finishedLoggingIn = true;
-            userInputs.set_AccessToken(AccessTokenString);
-
-
-            // Execute Facebook.Reading.User choreo.
-            User.UserResultSet userResults = userChoreo.execute(userInputs);
-
-            return userResults.get_Response();
-        } catch (Exception e) {
-            // if an exception occurred, log it
-            //Log.e(this.getClass().toString(), e.getMessage());
-        }
-        return null;
-    }
-
-    protected void onPostExecute(String userInfo) {
-        try {
-
-            JSONObject nytJSON = new JSONObject(userInfo);
-            final String id = nytJSON.get("id").toString();
-            final String userName = nytJSON.get("name").toString();
-            Log.i("ID: ", id);
-            Iterator<String> iter = nytJSON.keys();
-            while (iter.hasNext()) {
-                String key = iter.next();
-                try {
-                    if(!nytJSON.get(key).toString().contains("{"))
-                        Dictionary.privateInformation.add(nytJSON.get(key).toString());
-                    else{
-                        String[] s = nytJSON.get(key).toString().split(",");
-                        Dictionary.privateInformation.add(s[0].substring(1,s[0].length()-1));
-                        for(int i =1;i <s.length-1; ++i){
-                            Dictionary.privateInformation.add(s[i]);
-                        }
-                        Dictionary.privateInformation.add(s[s.length-1].substring(1,s[s.length-1].length()-1));
-                    }
-
-                    Log.i("Private Info: ", nytJSON.get(key) + "");
-                } catch (JSONException e) {
-                    // Something went wrong!
-                }
-            }
-
-            FacebookRegexPatternPool.id = id.toString();
-            FacebookRegexPatternPool.Name = userName.toString();
-
-            AfterLogin.webView.post(new Runnable() {
-                @Override
-                public void run() {
-                    // AfterLogin.webView.loadData("<html><body>Please Click one of the buttons at the top right to see Friend Requests and to post to your wall.</body></html>", "text/html; charset=UTF-8", null);
-                    AfterLogin.webView.loadUrl("https://www.facebook.com/" + FacebookRegexPatternPool.id);
-
-
-                    //AfterLogin.webView.loadUrl("https://www.google.com/");
-                }
-            });
-
-
-        } catch (Exception e) {
-            // if an exception occurred, show an error message
-            //Log.e("error:", e.toString());
-        }
-    }
-}
